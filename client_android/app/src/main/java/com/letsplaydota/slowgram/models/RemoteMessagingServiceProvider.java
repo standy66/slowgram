a3922c2a3190275f6c2fc8ffb6ed88fef5f3f0b1
kagudkov <kagudkov@gmail.com>
2015-04-29 23:55:35 +0300

    RemoteMessagingServiceProvider almost ready

--- client_android/app/src/main/java/com/letsplaydota/slowgram/models/RemoteMessagingServiceProvider.java
+++ client_android/app/src/main/java/com/letsplaydota/slowgram/models/RemoteMessagingServiceProvider.java
@@ -17,6 +17,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -54,7 +55,25 @@ public class RemoteMessagingServiceProvider implements MessagingServiceProvider 
 	private final String REQUEST_CODE_PHONE_FILED = "phone";
 	private final String REQUEST_CODE_DEVICE_ID_FIELD = "device_id";
 	private final String REQUEST_CODE_TOKEN_FIELD = "remember_token";
-	private final HttpRequestType REQUEST_CODE_HTTP_REQUEST_TYPE = HttpRequestType.POST;
+    private final String GET_CONTACT_LIST_ADDRESS = "/get_contact_list";
+    private final String GET_CONTACT_LIST_TOKEN_FIELD = "token";
+    private final String GET_CONTACT_LIST_NAME_OF_RETURN_ARRAY = "contacts";
+    private final String ADD_CONTACT_TO_CONTACT_LIST_ADDRESS = "/add_contact";
+    private final String ADD_CONTACT_TO_CONTACT_LIST_TOKEN_FIELD = "token";
+    private final String ADD_CONTACT_TO_CONTACT_LIST_ADD_PHONE_FIELD = "add_contact_phone";
+    private final String GET_DIALOG_ADDRESS = "/get_dialog";
+    private final String GET_DIALOG_TOKEN_FIELD = "token";
+    private final String GET_DIALOG_FROM_FIELD = "from";
+    private final String GET_DIALOG_TO_FIELD = "to";
+    private final String GET_DIALOG_NAME_OF_RETURN_ARRAY = "dialogs";
+    private final String CREATE_DIALOG_ADDRESS = "/create_dialog";
+    private final String CREATE_DIALOG_TOKEN_FIELD = "token";
+    private final String CREATE_DIALOG_PHONE_NUMBERS_FIELD = "phone_numbers";
+    private final String CREATE_DIALOG_DIALOG_ID = "dialogID";
+
+
+
+    private final HttpRequestType REQUEST_CODE_HTTP_REQUEST_TYPE = HttpRequestType.POST;
 
 	@Override
 	public void requestConfirmationCode(String phoneNumber, String deviceId) throws BadPhoneNumberException, ServerUnavailableException {
@@ -84,26 +103,148 @@ public class RemoteMessagingServiceProvider implements MessagingServiceProvider 
 
 	@Override
 	public Collection<Contact> getContactList(String token) throws BadTokenException {
-		return null;
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost sessionPostGetContactList = new HttpPost(remoteAddress + GET_CONTACT_LIST_ADDRESS);
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        nameValuePairs.add(new BasicNameValuePair(GET_CONTACT_LIST_TOKEN_FIELD, token));
+        try {
+            sessionPostGetContactList.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        HttpResponse response = null;
+        try {
+            response = httpClient.execute(sessionPostGetContactList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String body = null;
+        try {
+            body = EntityUtils.toString(response.getEntity());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(body);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Collection<Contact> contacts = new ArrayList<Contact>();
+        try {
+            contacts = (Collection<Contact>) jsonObject.get(GET_CONTACT_LIST_NAME_OF_RETURN_ARRAY);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return contacts;
 	}
 
 	@Override
 	public void addContactToContactList(String token, String contactPhoneNumber) throws ContactNotRegisteredException, BadTokenException {
-
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost sessionPostAddContact = new HttpPost(remoteAddress + ADD_CONTACT_TO_CONTACT_LIST_ADDRESS);
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        nameValuePairs.add(new BasicNameValuePair(ADD_CONTACT_TO_CONTACT_LIST_TOKEN_FIELD, token));
+        nameValuePairs.add(new BasicNameValuePair(ADD_CONTACT_TO_CONTACT_LIST_ADD_PHONE_FIELD, contactPhoneNumber));
+        try {
+            sessionPostAddContact.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        /*
+         * Кажется, это всё, нам не надо ничего от сервера получать
+         */
 	}
 
 	@Override
 	public List<Dialog> getDialogs(String token, int from, int to) throws BadTokenException {
-		return null;
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost sessionGetDialogs = new HttpPost(remoteAddress + GET_DIALOG_ADDRESS);
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        nameValuePairs.add(new BasicNameValuePair(GET_DIALOG_TOKEN_FIELD, token));
+        nameValuePairs.add(new BasicNameValuePair(GET_DIALOG_TO_FIELD, String.valueOf(from)));
+        nameValuePairs.add(new BasicNameValuePair(GET_DIALOG_FROM_FIELD, String.valueOf(to)));
+        try {
+            sessionGetDialogs.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        HttpResponse response = null;
+        try {
+            response = httpClient.execute(sessionGetDialogs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String body = null;
+        try {
+            body = EntityUtils.toString(response.getEntity());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(body);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        List<Dialog> dialogs = new ArrayList<Dialog>();
+        try {
+            dialogs = (List<Dialog>) jsonObject.get(GET_DIALOG_NAME_OF_RETURN_ARRAY);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return dialogs;
 	}
 
 	@Override
 	public String createDialog(String token, Collection<String> phoneNumbers) throws BadTokenException, BadPhoneNumberException {
-		return null;
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost sessionPostCreateDialog = new HttpPost(remoteAddress + CREATE_DIALOG_ADDRESS);
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        nameValuePairs.add(new BasicNameValuePair(CREATE_DIALOG_TOKEN_FIELD, token));
+        nameValuePairs.add(new BasicNameValuePair(CREATE_DIALOG_PHONE_NUMBERS_FIELD, new Gson().toJson(phoneNumbers)));
+        try {
+            sessionPostCreateDialog.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        HttpResponse response = null;
+        try {
+            response = httpClient.execute(sessionPostCreateDialog);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String body = null;
+        try {
+            body = EntityUtils.toString(response.getEntity());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(body);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String dialogID = null;
+        try {
+            dialogID = jsonObject.getString(CREATE_DIALOG_DIALOG_ID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return dialogID;
 	}
 
 	@Override
 	public void sendMessage(String token, String dialogId, String caption, String text) throws BadTokenException, BadDialogId {
+
 
 	}
 
