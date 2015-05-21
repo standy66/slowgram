283ba631992bee7ba2d2854ec3d681df76ea16e1
Gezort <Miron_55@mail.ru>
2015-05-22 01:51:55 +0400

    Redirecting to one message on full screen was added (Miron)

--- client_android/app/src/main/java/com/letsplaydota/slowgram/ContactMessagesFragment.java
+++ client_android/app/src/main/java/com/letsplaydota/slowgram/ContactMessagesFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 /**
@@ -21,7 +22,14 @@ public class ContactMessagesFragment extends Fragment {
         View v = inflater.inflate(R.layout.contact_messages_fragment, container, false);
         messagesListView = (ListView) v.findViewById(R.id.message_list_view);
         messagesListView.setAdapter(new ChatArrayAdapter(getActivity().getApplicationContext(), R.layout.single_message));
-
+        messagesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                getFragmentManager().beginTransaction()
+                        .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                        .replace(R.id.main_layout_fragment, new VievMessageFragment()).commit();
+            }
+        });
         return v;
     }
 }
---
+++ client_android/app/src/main/java/com/letsplaydota/slowgram/VievMessageFragment.java
@@ -0,0 +1,20 @@
+package com.letsplaydota.slowgram;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Мирон on 22.05.2015.
+ */
+public class VievMessageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
